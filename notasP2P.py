d3e0c477ebba7113774fa429939bb808f4821d60
Valdo-OPS <Satinodg.18@gmail.com>
2025-05-21 09:29:30 -0300

    Versión inicial del chat P2P

---
+++ notasP2P.py
@@ -0,0 +1,40 @@
+"""
+Quiero hacer un proyecto pequeño de programacion Lenguaje Python, Workbench Visual Studio Code, para el proyecto usare Django y Venv. Hagamos un simple chat P2P de principio basico 
+
+Una interfaz sencilla, Ventana de fondo negro liso 1020x800
+Dos botones, Unirse y Hostear 
+La opcion de Hostear debe utilizar UPNP para abrir el puerto de red, y luego devolver un codigo unico para que otro pueda unirse
+Y la opcion de Unirse debe pedir el codigo para conectarse con el Host.
+"""
+
+"""
+
+Usar Developer Command prompt for VS
+
+"""
+
+"""
+#Creacion del entorno con python 3.11 de Windows
+(El 3.10 es de Linux-like)
+
+"E:\Programacion\Python-3.11.11\python.exe" -m venv venv
+
+call venv\Scripts\activate.bat
+
+"""
+
+"""
+
+# --- Bugs a resolver ---
+
+La limpieza de botones entre Unirse y Conectar
+Al continuamente cambiar entre ellos se buggea y no muestra ningun label
+(SOLUCIONADO)
+
+Al "hostear" se abre la ventana de chat incluso apretando el boton de "volver"
+(SOLUCIONADO)
+
+No deja de incrementar los puertos y no los cierra cada vez que se "Hostea"
+(SOLUCIONADO)
+
+"""
